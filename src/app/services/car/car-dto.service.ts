0059c0a37437f6889ec243c1f9659451944c41b6
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2022-02-09 17:09:16 +0300

    ✅ adding get methot through api

--- src/app/services/car/car-dto.service.ts
+++ src/app/services/car/car-dto.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CarDto } from 'src/app/models/CarDto';
+import { listResponseModel } from 'src/app/models/ListResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarDtoService {
+   apiUrl = "https://localhost:44309/api/";
+  constructor(private httpClient : HttpClient) { }
 
-  constructor() { }
+  getCarDtos():Observable<listResponseModel<CarDto>>{
+    let newPath =this.apiUrl+"cars/getCarDetails"
+    return this.httpClient.get<listResponseModel<CarDto>>(newPath)
+  }
 }

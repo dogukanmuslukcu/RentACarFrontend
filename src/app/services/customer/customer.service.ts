91c56b27880139a2a7ff32a4252da13800117f53
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2022-02-09 17:15:12 +0300

    ✅ adding get methot through api

--- src/app/services/customer/customer.service.ts
+++ src/app/services/customer/customer.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Customer } from 'src/app/models/Customer';
+import { listResponseModel } from 'src/app/models/ListResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  apiUrl = "https://localhost:44309/api/customers/getAll";
+  constructor(private httpClient: HttpClient) { }
+
+  getCustomers():Observable<listResponseModel<Customer>> {
+    return this.httpClient.get<listResponseModel<Customer>>(this.apiUrl)
+  }
+
 }

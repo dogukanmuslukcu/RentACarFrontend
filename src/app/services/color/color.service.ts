dcfb28ecadc4d427cac65c6b48ee37aa8d79d60c
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2022-02-09 17:13:41 +0300

    ✅ adding get methot through api

--- src/app/services/color/color.service.ts
+++ src/app/services/color/color.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Color } from 'src/app/models/color';
+import { listResponseModel } from 'src/app/models/ListResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColorService {
 
-  constructor() { }
+  apiUrl = "https://localhost:44309/api/colors/getAll";
+  constructor(private httpClient :HttpClient) { }
+
+  getColors():Observable<listResponseModel<Color>>{
+    return this.httpClient.get<listResponseModel<Color>>(this.apiUrl)
+  }
 }

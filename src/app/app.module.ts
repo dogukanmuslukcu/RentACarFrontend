0a7a6bdd004282eb121a19d501a9aae70347b42e
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2022-02-09 16:11:13 +0300

    created components=>( brand, color, customer, car-dto,rental-dto)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { CarDtoComponent } from './components/car-dto/car-dto.component';
+import { RentalDtoComponent } from './components/rental-dto/rental-dto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent,
+    ColorComponent,
+    CustomerComponent,
+    CarDtoComponent,
+    RentalDtoComponent
   ],
   imports: [
     BrowserModule,

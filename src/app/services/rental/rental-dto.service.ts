57e8f5ed696ef43d228d6b54d32b861b436a9762
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2022-02-09 17:30:15 +0300

    ✅ added get methot through api

--- src/app/services/rental/rental-dto.service.ts
+++ src/app/services/rental/rental-dto.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { listResponseModel } from 'src/app/models/ListResponseModel';
+import { RentalDto } from 'src/app/models/RentalDto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RentalDtoService {
 
-  constructor() { }
+  apiUrl = "https://localhost:44309/api/rentals/";
+  constructor(private httpClient: HttpClient) { }
+
+  getRentalDtos():Observable<listResponseModel<RentalDto>>{
+     let newpath =this.apiUrl+"getrentaldetails";
+    return this.httpClient.get<listResponseModel<RentalDto>>(newpath)
+
+  }
+
+
 }

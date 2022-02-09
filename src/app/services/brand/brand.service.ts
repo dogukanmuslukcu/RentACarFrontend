015c5f3c1baeefbdfc75700b224d5ab7df21a842
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2022-02-09 17:08:40 +0300

    ✅ adding get methot through api

--- src/app/services/brand/brand.service.ts
+++ src/app/services/brand/brand.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+import { Observable } from 'rxjs';
+import { listResponseModel } from 'src/app/models/ListResponseModel';
+import { Brand } from 'src/app/models/Brand';
 @Injectable({
   providedIn: 'root'
 })
 export class BrandService {
 
-  constructor() { }
+  apiUrl = "https://localhost:44309/api/brands/getAll";
+  constructor(private httpClient : HttpClient) { }
+
+  getBrands():Observable<listResponseModel<Brand>>{
+    return this.httpClient.get<listResponseModel<Brand>>(this.apiUrl)
+  }
 }

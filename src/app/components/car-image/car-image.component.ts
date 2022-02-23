fa2f26ce45418960b9b9f1c7c851848f53af14fe
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2022-02-23 23:43:00 +0300

    ✅ Added rental-add (finally ✨ )

--- src/app/components/car-image/car-image.component.ts
+++ src/app/components/car-image/car-image.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { CarDto } from 'src/app/models/carDto';
 import { CarImage } from 'src/app/models/carImage';
 import { CarImageDto } from 'src/app/models/carImageDto';
@@ -17,8 +18,15 @@ export class CarImageComponent implements OnInit {
 carImages:CarImage[]=[];
 carDtos:CarDto[]=[];
 carImageDtos:CarImageDto[]=[];
+isCarRent:boolean
 
-  constructor(private carImageService:CarImageService,private carDtoService:CarDtoService,private carImageDtoService:CarImageDtoService, private activatedRoute :ActivatedRoute) { }
+  constructor(
+    private carImageService:CarImageService,
+    private carDtoService:CarDtoService,
+    private carImageDtoService:CarImageDtoService,
+     private activatedRoute :ActivatedRoute,
+     private toastrService: ToastrService
+     ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params =>{
@@ -46,9 +54,19 @@ carImageDtos:CarImageDto[]=[];
   getCarDtoByCarId(carId:number){
     this.carDtoService.getCarDtosByCarId(carId).subscribe(response=>{
     this.carDtos = response.data;
+    this.carDtos.map(c=>c.isCarRentable?this.isCarRent=true:this.isCarRent=false)
     })
   }
 
+  setIsCarRentable(){
+    if ( this.isCarRent){
+      this.isCarRent=true
+      this.toastrService.success("Araç kiralanabilir");
+    } else {
+      this.isCarRent=false
+      this.toastrService.error("Araç kiralanabilir");
+    }
+  }
   getCarImageDtoByCarId(carId:number){
 this.carImageDtoService.getCarImageDtoByCarId(carId).subscribe(response=>{
   this.carImageDtos=response.data

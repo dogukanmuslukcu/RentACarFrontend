907d352a9dd0eb03e937195f0794540ead09a264
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2022-02-23 22:29:05 +0300

    ✅ Added Payment for Add-rental

--- src/app/components/rental-add/rental-add.component.ts
+++ src/app/components/rental-add/rental-add.component.ts
@@ -1,9 +1,10 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import {FormGroup,FormBuilder,FormControl,Validators} from "@angular/forms"
 import { ToastrService } from 'ngx-toastr';
 import { RentalDtoService } from 'src/app/services/rental/rental-dto.service';
+import { PaymentService } from 'src/app/services/payment/payment.service';
 
 @Component({
   selector: 'app-rental-add',
@@ -13,13 +14,16 @@ import { RentalDtoService } from 'src/app/services/rental/rental-dto.service';
 export class RentalAddComponent implements OnInit {
 
   CarRentForm:FormGroup;
-carId:Number;
+  CreditCardForm:FormGroup;
+  carId:Number;
+
   constructor(
     private formBuilder :FormBuilder,
     private datePipe :DatePipe,
     private activatedRoute: ActivatedRoute,
-    private rentalService:RentalDtoService,
     private toastrService:ToastrService,
+    private rentalService:RentalDtoService,
+    private paymentService:PaymentService
     ) { }
 
   ngOnInit(): void {
@@ -27,29 +31,69 @@ carId:Number;
       this.carId = Number(params['carId']);
   })
   this.createCarRentForm();
+  this.createCreditCardForm();
   }
   createCarRentForm(){
     this.CarRentForm =this.formBuilder.group({
-      CarId:[this.carId],
-      CustomerId:["",Validators.required],
-      RentDate:[this.datePipe.transform(new Date(),'yyyy-MM-dd'), Validators.required],
-      ReturnDate:[this.datePipe.transform(new Date(),'yyyy-MM-dd'),Validators.required]
+      carId:[this.carId],
+      customerId:["",Validators.required],
+      rentDate:[this.datePipe.transform(new Date(),'yyyy-MM-dd'), Validators.required],
+      returnDate:[this.datePipe.transform(new Date(),'yyyy-MM-dd'),Validators.required]
     })
   }
 
-  rent(){
- if(this.CarRentForm.valid){
-      let carModel = Object.assign({},this.CarRentForm.value);
-      this.rentalService.rent(carModel).subscribe(response=>{
-        console.log(response)
-        this.toastrService.success(response.message,"Ödeme adımına yönlendiriliyorsunuz.")
-      },responseError=>{
-        console.log(responseError)
-        this.toastrService.error(responseError.error)
-      })
-    }else{
-      this.toastrService.error("Formunuz eksik veya hatalı.")
-    }
+  createCreditCardForm(){
+    this.CreditCardForm =this.formBuilder.group({
+      NameAndSurname:["",Validators.required],
+      CartNumber:["",Validators.required],
+      ExpMonth:[0,Validators.required],
+      ExpYear:[0,Validators.required],
+      Cvc:["",Validators.required]
+    })
+  }
+  payment(){
+
+   if(this.CreditCardForm.valid){
+    let creditModel =Object.assign({},this.CreditCardForm.value);
+
+    this.paymentService.payment(creditModel).subscribe(response=>{
+      this.toastrService.success(response.message,"Ödeme Başarıyla Tamamlandı.")
+if(response.success){
+  if(this.CarRentForm.valid){
+    let carModel = Object.assign({},this.CarRentForm.value);
+    this.rentalService.rent(carModel).subscribe(response=>{
+
+      this.toastrService.success(response.message,"Başarılı!")
+
+    },responseError=>{
+      if(responseError.error.Errors.length>0){
+        for (let i = 0; i <responseError.error.Errors.length; i++) {
+          this.toastrService.error(responseError.error.Errors[i].ErrorMessage
+            ,"Doğrulama hatası")
+        }
+      }
+    })
+
+  }else{
+    this.toastrService.error("Formunuz eksik veya hatalı.")
 
   }
+}
+    },responseError=>{
+      if(responseError.error.Errors.length>0){
+        for (let i = 0; i <responseError.error.Errors.length; i++) {
+          this.toastrService.error(responseError.error.Errors[i].ErrorMessage
+            ,"Doğrulama hatası")
+        }
+      }
+    })
+
+  }
+     else{
+      this.toastrService.error("Ödeme sırasında hata alındı.")
+    }
+  }
+
+
+
 }
